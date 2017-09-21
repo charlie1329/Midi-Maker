3363a3af4db3577c78ff9fa24bb22e2739e69412
charlie1329 <charlie.street1@btinternet.com>
2017-09-21 17:22:24 +0100

    First commit. Need to test functionality

---
+++ cmake-build-debug/CMakeFiles/MidiMaker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MidiMaker.dir/midi.cpp.obj"
+  "MidiMaker.pdb"
+  "MidiMaker.exe"
+  "MidiMaker.exe.manifest"
+  "libMidiMaker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MidiMaker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
